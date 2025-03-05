c6c3b358f3b6814a6075e7c6aa492305f105e241
daoujaejin <jaejin@daou.co.kr>
2025-03-05 13:38:21 +0900

    Create [c]250305-실습3번.c

---
+++ [c]250305-실습3번.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+	// 실습 1번
+	printf("=================\n");
+	printf("학과: 경영학부\n");
+	printf("이름: 이재진\n");
+	printf("학번: 2018027938\n");
+	printf("강사: 정현희\n");
+	printf("=================\n");
+
+	// 실습 2번
+	printf("이름 이재진, 목표 고수 개발자, 애칭 ??\n");
+
+	return 0;
+}
