8894c31327a254d872cd9a6ec68184fd5a202501
daoujaejin <jaejin@daou.co.kr>
2025-03-25 11:38:55 +0900

    Delete [c]250325 실습 directory

--- [c]250325 실습/sql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE transactions (
-    customer_name VARCHAR2(50) NOT NULL,
-    stock_name VARCHAR2(50) NOT NULL,
-    trade_type VARCHAR2(20) NOT NULL,
-    quantity NUMBER(10) NOT NULL,
-    price NUMBER(15, 2) NOT NULL
-);
